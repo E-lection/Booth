39a734d161c8c5760e24fdd769c0da7f7fa328f0
Jennifer Lea <jennifer.lea15@imperial.ac.uk>
2017-05-31 15:50:20 +0100

    Added booth stuff to views.

--- e_lection/views.py
+++ e_lection/views.py
@@ -8,14 +8,29 @@ from forms import FindVoterForm
 def index():
     return render_template('index.html')
 
+################## STATION STUFF ##################
 
-@app.route('/station')
-def station():
-    return render_template('station.html')
+# @app.route('/station')
+# def station():
+#     return render_template('station.html')
+#
+# @app.route('/station', methods=['POST'])
+# def find_voter():
+#     form = FindVoterForm()
+#     if form.validate_on_submit():
+#         return render_template('voterdb.html', form = form)
+#     return render_template('station.html', form = form)
 
-@app.route('/station', methods=['POST'])
-def find_voter():
-    form = FindVoterForm()
-    if form.validate_on_submit():
-        return render_template('voterdb.html', form = form)
-    return render_template('station.html', form = form)
+#################################################
+
+################## BOOTH STUFF ##################
+
+@app.route('/booth_enter_pin')
+def booth_enter_pin():
+    return render_template('booth_enter_pin.html')
+
+@app.route('/booth_cast_vote')
+def booth_cast_vote():
+    return render_template('booth_cast_vote.html')
+
+#################################################
