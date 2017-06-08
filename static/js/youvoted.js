93ae21f26d1e12954ce46495357a1e390d38346b
Rishabh Jain <wakeuprj@gmail.com>
2017-06-08 12:12:14 +0100

    Commented back in youvoted.js.

--- static/js/youvoted.js
+++ static/js/youvoted.js
@@ -1,11 +1,11 @@
-// window.onload = function(){
-//   var counter = document.getElementById('count-down');
-//   setInterval(function() {
-//     counter--;
-//     if(counter < 0) {
-//       window.location.replace('/');
-//     } else {
-//       document.getElementById('count-down').innerHTML = counter;
-//     }
-//   }, 5000);
-// }
+window.onload = function(){
+  var counter = document.getElementById('count-down');
+  setInterval(function() {
+    counter--;
+    if(counter < 0) {
+      window.location.replace('/');
+    } else {
+      document.getElementById('count-down').innerHTML = counter;
+    }
+  }, 5000);
+}
